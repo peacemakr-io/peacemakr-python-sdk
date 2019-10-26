c648f84a267e7ab98fbac13bb2f4d3217e563d35
Clyde <caradine.clyde@gmail.com>
2019-10-26 14:39:46 +0200

    Remove rm of java generated stubs.

--- generate-stub-client.sh
+++ generate-stub-client.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rm -rf src/test/java/io/peacemakr/crypto/swagger || true
-
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.5 generate \
     -i local/peacemakr-services.yml \
     -l python \
