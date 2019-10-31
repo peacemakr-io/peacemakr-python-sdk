c1fc1d872ba812dbc533a2843ba9bcf2faf00236
max <Maxime Cuny>
2019-10-31 11:38:27 +0100

    fixed problem in generation command

--- generate-stub-client.sh
+++ generate-stub-client.sh
@@ -3,5 +3,5 @@
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.5 generate \
     -i local/peacemakr-services.yml \
     -l python \
-    --api-package io.peacemakr.crypto.swagger.client.api \
+    -o local
 
