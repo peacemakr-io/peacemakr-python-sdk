a576ffc8acbf5174db9ddbaef0ffe0348a9f5afb
max <Maxime Cuny>
2019-11-14 22:28:06 +0100

    [add] sync prototype

--- peacemakr_sdk/impl/crypto_impl.py
+++ peacemakr_sdk/impl/crypto_impl.py
@@ -2,11 +2,15 @@ from peacemakr_sdk.generated.api_client import ApiClient
 from peacemakr_sdk.generated.api.client_api import ClientApi
 from peacemakr_sdk.generated.api.crypto_config_api import CryptoConfigApi
 from peacemakr_sdk.generated.api.org_api import OrgApi
+from peacemakr_sdk.generated.api.key_service_api import KeyServiceApi
 
 from peacemakr_sdk.generated.models.client import Client
 from peacemakr_sdk.generated.configuration import Configuration
 from peacemakr_sdk.generated.models import *
+
+from peacemakr_sdk.generated.rest import ApiException
 from peacemakr_sdk.crypto_base import PeacemakrCryptoSDK
+
 import peacemakr_core_crypto_python as p
 
 import time
@@ -25,8 +29,26 @@ PERSISTER_APIKEY_KEY = "ApiKey"
 DEFAULT_SYMM_CIPHER = p.SymmetricCipher.CHACHA20_POLY1305
 
 class CryptoImpl(PeacemakrCryptoSDK):
-
     def __init__(self, api_key="", client_name="", peacemakr_hostname="", persister=None, logger=None):
+        '''
+            This is the CryptoSDK constructor.
+            Public Attributes are:
+                api_key: str: api key accessible from admin portal
+                client_name: str: the clients name
+                sdk_version: str: version number of the current SDK
+                peacemakr_hostname: str: the SDK hostname
+                org: obj: the organization's information
+                crypto_config: obj: the current local crypto configuration
+                persister: obj: the persister
+                logger: obj: the logger API
+                client: obj: the client API
+
+            Private Attributes are:
+                api_cient:
+                authentication:
+                loaded_private_preferred_key:
+                loaded_private_preferred_cipher:
+        '''
         self.api_key = api_key
         self.client_name = client_name
         self.sdk_version = "0.0.1"
@@ -142,8 +164,58 @@ class CryptoImpl(PeacemakrCryptoSDK):
 
     def __verify_bootstrapped_and_registered(self):
         # FIXME: the exception needs to be peacemakr specific
-        if not (self.__is_registered() and self.__is_bootstrapped()):
+        if not self.__is_registered() or not self.__is_bootstrapped():
             raise Exception("SDK was not registered, please register before using other SDK operations.")
+
+    def __save_new_aymmetric_key_pair(self, src, dst):
+        dst.save(PERSISTER_PRIV_KEY, src.load(PERSISTER_PRIV_KEY));
+        dst.save(PERSISTER_PUB_KEY, src.load(PERSISTER_PUB_KEY));
+        dst.save(PERSISTER_ASYM_TYPE, src.load(PERSISTER_ASYM_TYPE));
+        dst.save(PERSISTER_ASYM_CREATED_DATE_EPOCH, src.load(PERSISTER_ASYM_CREATED_DATE_EPOCH));
+        dst.save(PERSISTER_ASYM_BITLEN, src.load(PERSISTER_ASYM_BITLEN));
+
+    def __gen_and_register_new_preferred_client_key(self):
+        print("Generating a new preferred client key")
+        temp_in_memory_persister: InMemoryPersister = InMemoryPersister()
+        public_key = self.__gen_new_asymmetric_keypair(temp_in_memory_persister)
+
+        print("Registering the new public key")
+        client_api = ClientAPI(self.__get_client())
+        try:
+            public_key = client_api.addClientPublicKey(this.client.getId(), public_key)
+        except ApiException as e:
+            print(e)
+            pass
+
+        print("Successfully registered new public key as client preferred key")
+        self.__save_new_aymmetric_key_pair(temp_in_memory_persister, self.persister)
+        self.persister.save(PERSISTER_PREFERRED_KEYID, public_key.getId())
+
+        print("Successfully saved new public key as client preferred key")
+
+    def __update_local_crypto_config(self, new_config):
+        cur_asymmetric_key_type = self.persister.load(PERSISTER_ASYM_TYPE)
+        if not new_config.getClientKeyType().equals(cur_asymmetric_key_type):
+            #FIXME: add logger
+            self.crypto_config = new_config
+            self.__gen_and_register_new_preferred_client_key()
+
+        cur_asymmetric_key_creation_time = self.persister.load(PERSISTER_ASYM_CREATED_DATE_EPOCH)
+        asymmetric_key_creation_time = long(cur_asymmetric_key_creation_time)
+        if (new_config.getClientKeyTTL() + asymmetric_key_creation_time) > int(round(time.time())):
+            #FFIXME: add logger
+            self.crypto_config = new_config
+            self.__gen_and_register_new_preferred_client_key()
+
+        cur_asymmetric_key_bit_lens = self.persister.load(PERSISTER_ASYM_BITLEN);
+        asymmertric_key_bit_len = int(cur_asymmetric_key_bit_lens)
+        if asymmertric_key_bit_len != new_config.getClientKeyBitlength():
+            #FIXME: add logger
+            self.crypto_config = new_config
+            self.__gen_and_register_new_preferred_client_key()
+
+        self.crypto_config = new_config
+
 
     def register(self):
         # check is register and is boostrap, if not initialize
@@ -173,10 +245,22 @@ class CryptoImpl(PeacemakrCryptoSDK):
 
         # save info in persister
 
+
     def sync(self):
-        print("In sync")
+        self.__verify_bootstrapped_and_registered()
+        crypto_config_api = CryptoConfigApi(self.__get_client())
+
+        try:
+            new_config = crypto_config_api.get_crypto_config(self.crypto_config.id)
+            if not new_config == self.crypto_config:
+                self.__update_local_crypto_config(new_config)
+        except ApiException as e:
+            #FIXME: create server exception
+            raise ServerException(e)
         pass
 
+        # FIXME: uncomment when implemented
+        # self.__download_and_save_all_keys()
 
     def encrypt(self, plain_text: bytes) -> bytes:
         print("In encrypt")
