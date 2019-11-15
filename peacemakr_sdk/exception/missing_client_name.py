8cd94de027c713d1bf9ab0e332ee8d3f975f2f9c
max <Maxime Cuny>
2019-11-15 17:23:19 +0100

    [ADD] added dummy implem for peacemakr exceptions

---
+++ peacemakr_sdk/exception/missing_client_name.py
@@ -0,0 +1,4 @@
+from peacemakr_sdk.exception.peacemakr import PeacemakrError
+
+class MissingClientNameError(PeacemakrError):
+  pass
---
+++ peacemakr_sdk/exception/persistence_layer_corruption_detected.py
@@ -0,0 +1,4 @@
+from peacemakr_sdk.exception.peacemakr import PeacemakrError
+
+class PersistenceLayerCorruptionDetectedError(PeacemakrError):
+  pass
