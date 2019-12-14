34457da9b596f7d3f8c62e70725ee0f6f0b0aa4f
Daniel Huang <yhung119@illinois.edu>
2019-12-14 03:54:24 -0600

    remove aws profile name

--- build-test.sh
+++ build-test.sh
@@ -5,7 +5,6 @@ set -ex
 # Setup credentials
 $(aws ecr get-login --no-include-email --region us-east-2) || true
 
-
 # Pull the latest
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-services:latest || true
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/key-derivation-service:latest || true
