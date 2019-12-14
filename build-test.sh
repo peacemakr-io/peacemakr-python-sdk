157f1ee2f4e31ec0632b227450fcbe4f8a51e5a6
Daniel Huang <yhung119@illinois.edu>
2019-12-14 03:54:24 -0600

    remove aws profile name

--- build-test.sh
+++ build-test.sh
@@ -3,42 +3,12 @@
 set -ex
 
 # Setup credentials
-<<<<<<< HEAD
+
 $(aws ecr get-login --no-include-email --region us-east-2) || true
-=======
-$(aws ecr get-login --no-include-email --region us-east-2 --profile peacemakr) || true
->>>>>>> test aws key
 
 # Pull the latest
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-services:latest || true
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/key-derivation-service:latest || true
-<<<<<<< HEAD
 docker pull danielhuang37/peacemakr-pycmakessl:latest || true
-=======
-
-# docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
-# test_volume_exists=$(docker volume ls | grep python-test-volume)
-# if [ -z "$test_volume_exists" ]; then
-#   docker volume create python-test-volume
-# fi
-
-# mkdir output 2> /dev/null
-# directory_coverage_exists=$(ls | grep output/coverage)
-# directory_tests_exists=$(ls | grep output/tests)
-# directory_flake8_exists=$(ls | grep output/flake8)
-# if [ -z "$directory_coverage_exists" ]; then
-#   mkdir output/coverage
-# fi
-# if [ -z "$directory_flake8_exists" ]; then
-#   mkdir output/flake8
-# fi
-# if [ -z "$directory_tests_exists" ]; then
-#   mkdir output/tests
-# fi
-
-# docker run --network='host' \
-#            -v $(pwd)/output/:/peacemakr/python/output/ \
-#            peacemakr-python-sdk-test
->>>>>>> test aws key
 
 docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
