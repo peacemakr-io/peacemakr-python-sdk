b54ba218ad5f49396fcad63a437ada539cfb049e
Daniel Huang <yhung119@illinois.edu>
2019-12-14 04:08:51 -0600

    add docker-comopse and use pre-built python with cmakessl

--- build-test.sh
+++ build-test.sh
@@ -8,8 +8,9 @@ $(aws ecr get-login --no-include-email --region us-east-2) || true
 # Pull the latest
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-services:latest || true
 docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/key-derivation-service:latest || true
+docker pull danielhuang37/peacemakr-pycmakessl:latest || true
 
-# docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
+docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
 # test_volume_exists=$(docker volume ls | grep python-test-volume)
 # if [ -z "$test_volume_exists" ]; then
 #   docker volume create python-test-volume
