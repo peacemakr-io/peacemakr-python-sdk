4a7b1c6039e36ee9d3b52628540cbc985e6c93e1
Daniel Huang <yhung119@illinois.edu>
2020-01-18 12:19:30 +0800

    clean up comments

--- build-test.sh
+++ build-test.sh
@@ -11,26 +11,3 @@ docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/key-derivation-service:
 docker pull danielhuang37/peacemakr-pycmakessl:latest || true
 
 docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
-# test_volume_exists=$(docker volume ls | grep python-test-volume)
-# if [ -z "$test_volume_exists" ]; then
-#   docker volume create python-test-volume
-# fi
-
-# mkdir output 2> /dev/null
-# directory_coverage_exists=$(ls | grep output/coverage)
-# directory_tests_exists=$(ls | grep output/tests)
-# directory_flake8_exists=$(ls | grep output/flake8)
-# if [ -z "$directory_coverage_exists" ]; then
-#   mkdir output/coverage
-# fi
-# if [ -z "$directory_flake8_exists" ]; then
-#   mkdir output/flake8
-# fi
-# if [ -z "$directory_tests_exists" ]; then
-#   mkdir output/tests
-# fi
-
-# docker run --network='host' \
-#            -v $(pwd)/output/:/peacemakr/python/output/ \
-#            peacemakr-python-sdk-test
-
