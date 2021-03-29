37f9823b51ddb59fcbf0f397df711583205c28b9
Daniel Huang <danielhuang37@outlook.com>
2021-03-28 18:42:50 -0700

    update docker register and key deriver config (#39)

--- build-test.sh
+++ build-test.sh
@@ -2,12 +2,9 @@
 
 set -ex
 
-# Setup credentials
-$(aws ecr get-login --no-include-email --region us-east-2) || true
-
 # Pull the latest
-docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-services:latest || true
-docker pull 716293438869.dkr.ecr.us-east-2.amazonaws.com/key-derivation-service:latest || true
+docker pull gcr.io/peacekube/peacemakr-services:latest || true
+docker pull gcr.io/peacekube/key-derivation-service:latest || true
 docker pull danielhuang37/peacemakr-pycmakessl:latest || true
 
 docker build -t peacemakr-python-sdk-test -f TestDockerfile --network=host .
